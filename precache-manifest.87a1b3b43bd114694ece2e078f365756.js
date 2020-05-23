708fcca6abd984ce907e5d27744993b5fb3d7154
Oscar <oscar.ramos@ucsp.edu.pe>
2020-05-23 13:21:25 -0500

    Updates

---
+++ precache-manifest.87a1b3b43bd114694ece2e078f365756.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bb815623b9b665c851ba125ec00fe7a",
+    "url": "/caratulas-ucsp/index.html"
+  },
+  {
+    "revision": "4e2611f783aba7bdb164",
+    "url": "/caratulas-ucsp/static/js/2.e835a4fd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/caratulas-ucsp/static/js/2.e835a4fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "758f61465c4eafb68ee2",
+    "url": "/caratulas-ucsp/static/js/main.73e33f30.chunk.js"
+  },
+  {
+    "revision": "0b5561329d92f12542c4",
+    "url": "/caratulas-ucsp/static/js/runtime-main.1f545457.js"
+  }
+]);
