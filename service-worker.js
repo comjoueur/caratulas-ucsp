37361f0927c960802591b314a1c01f0fa2e1f55d
oscaramos <oscar.ramos@ucsp.edu.pe>
2020-06-10 03:32:53 +0000

    Deploying to gh-pages from  @ 2e7767d136999495ba5fd9a5aa9ec106dacd7875 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/caratulas-ucsp/precache-manifest.3d60d95a95a8eaa8744bb3a06fa865d1.js"
+  "/caratulas-ucsp/precache-manifest.520e9f9d3d7c0c31f2674a4210c8c507.js"
 );
 
 self.addEventListener('message', (event) => {
